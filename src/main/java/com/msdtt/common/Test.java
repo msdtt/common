fdfb31e15f6aed73ca91ed60d1f939523c20f727
竺旭栋 <zhuxd@wjs.com>
2017-08-12 22:27:48 +0800

    初始化文件

---
+++ src/main/java/com/msdtt/common/Test.java
@@ -0,0 +1,12 @@
+package com.msdtt.common;
+
+/**
+ * @author zhuxd@wjs.com
+ * @date 2017/8/12 22:22
+ */
+public class Test{
+
+	public static void main(String[] args){
+		System.out.println("x");
+	}
+}
